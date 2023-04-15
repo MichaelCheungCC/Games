996190b25edde3c8263edf596d347784180600e1
Michael Cheung <chichung19961129@gmail.com>
2023-04-15 23:09:04 +0800

    Finished memory game

--- MemoryGame/app.js
+++ MemoryGame/app.js
@@ -1,66 +1,142 @@
 const cardArray = [
     {
-        name: 'fries',
-        img: 'images/fries.png'
+        name: 'Angry',
+        img: 'images/Angry.png'
     },
     {
-        name: 'cheeseburger',
-        img: 'images/cheeseburger.png'
+        name: 'Crying',
+        img: 'images/Crying.png'
     },
     {
-        name: 'hotdog',
-        img: 'images/hotdog.png'
+        name: 'Dizzy',
+        img: 'images/Dizzy.png'
     },
     {
-        name: 'ice-cream',
-        img: 'images/ice-cream.png'
+        name: 'Happy',
+        img: 'images/Happy.png'
     },
     {
-        name: 'milkshake',
-        img: 'images/milkshake.png'
+        name: 'Hungry',
+        img: 'images/Hungry.png'
     },
     {
-        name: 'pizza',
-        img: 'images/pizza.png'
+        name: 'Loudly Crying',
+        img: 'images/Loudly Crying.png'
     },
     {
-        name: 'fries',
-        img: 'images/fries.png'
+        name: 'Omg',
+        img: 'images/Omg.png'
     },
     {
-        name: 'cheeseburger',
-        img: 'images/cheeseburger.png'
+        name: 'Sleeping',
+        img: 'images/Sleeping.png'
     },
     {
-        name: 'hotdog',
-        img: 'images/hotdog.png'
+        name: 'Tears',
+        img: 'images/Tears.png'
     },
     {
-        name: 'ice-cream',
-        img: 'images/ice-cream.png'
+        name: 'Very Mad',
+        img: 'images/Very Mad.png'
     },
     {
-        name: 'milkshake',
-        img: 'images/milkshake.png'
+        name: 'Angry',
+        img: 'images/Angry.png'
     },
     {
-        name: 'pizza',
-        img: 'images/pizza.png'
+        name: 'Crying',
+        img: 'images/Crying.png'
     },
+    {
+        name: 'Dizzy',
+        img: 'images/Dizzy.png'
+    },
+    {
+        name: 'Happy',
+        img: 'images/Happy.png'
+    },
+    {
+        name: 'Hungry',
+        img: 'images/Hungry.png'
+    },
+    {
+        name: 'Loudly Crying',
+        img: 'images/Loudly Crying.png'
+    },
+    {
+        name: 'Omg',
+        img: 'images/Omg.png'
+    },
+    {
+        name: 'Sleeping',
+        img: 'images/Sleeping.png'
+    },
+    {
+        name: 'Tears',
+        img: 'images/Tears.png'
+    },
+    {
+        name: 'Very Mad',
+        img: 'images/Very Mad.png'
+    }
 ]
 
 cardArray.sort(() => 0.5 - Math.random())
 
 const gridDisplay = document.querySelector('#grid')
+var score = 0
+let cardsChosenName = []
+let cardsChosenIds = []
+const cardsWon = []
 
-function createBoard () {
-    for (let i = 0; i < 12; i++) {
+function createBoard() {
+    for (let i = 0; i < cardArray.length; i++) {
         const card = document.createElement('img')
         card.setAttribute('src', 'images/blank.png')
+        card.setAttribute('height', '80px')
+        card.setAttribute('width', '80px')
         card.setAttribute('data-id', i)
+        card.addEventListener('click', flipboard)
         gridDisplay.appendChild(card)
-
-        console.log(card, i)
     }
 }
 createBoard()
+
+function checkMatch() {
+    const cards = document.querySelectorAll('img')
+    const optionOneId = cardsChosenIds[0]
+    const optionTwoId = cardsChosenIds[1]
+    if (optionOneId == optionTwoId) {
+        alert('You have clicked the same image!')
+    }
+    if (cardsChosenName[0] == cardsChosenName[1]) {
+        alert('You found a match!')
+        cards[optionOneId].setAttribute('src', 'images/white.png')
+        cards[optionTwoId].setAttribute('src', 'images/white.png')
+        cards[optionOneId].removeEventListener('click', flipboard)
+        cards[optionTwoId].removeEventListener('click', flipboard)
+        cardsWon.push(cardsChosenName[0])
+        score += 2
+    }
+    if (cardsChosenName[0] != cardsChosenName[1]) {
+        alert('Try again!')
+        cards[optionOneId].setAttribute('src', 'images/blank.png')
+        cards[optionTwoId].setAttribute('src', 'images/blank.png')
+        score -= 1
+    }
+    cardsChosenName = []
+    cardsChosenIds = []
+    document.getElementById('result').innerHTML = score
+    if (cardsWon.length == cardArray.length/2)
+        alert(`Congratulations! You finished the game. Your score is ${score}!`)
+}
+
+function flipboard() {
+    const cardId = this.getAttribute('data-id')
+    cardsChosenName.push(cardArray[cardId].name)
+    cardsChosenIds.push(cardId)
+    this.setAttribute('src', cardArray[cardId].img)
+    if (cardsChosenName.length === 2) {
+        setTimeout(checkMatch, 500)
+    }
+}
